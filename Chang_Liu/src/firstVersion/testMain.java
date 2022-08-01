cb7217bb02a879ea9555c92d1adab3db2af269a0
Chang_Liu <PC@LAPTOP-0LGMH5S5>
2022-08-01 19:17:14 +0800

    first try

---
+++ Chang_Liu/src/firstVersion/testMain.java
@@ -0,0 +1,11 @@
+package firstVersion;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("1");
+	}
+
+}
---
+++ Chang_Liu/src/module-info.java
@@ -0,0 +1,2 @@
+module Chang_Liu {
+}
